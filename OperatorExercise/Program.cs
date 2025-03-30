ee6cd463ca5945f4e84c489d0eeb2961819a3909
justin brewer <justinbrewer49@gmail.com>
2025-03-30 16:07:39 -0400

    operationsexercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($" {a} + {b} is {sum}");
+            Console.WriteLine($"{a} - {b} is {difference}");
+            Console.WriteLine($" {a} * {b} is {product}");
+            Console.WriteLine($" {a} / {b} is {quotient}");
+            Console.WriteLine($" {a} % {b} is {remainder};");
+
+            AreaOfCircle();
+        }
+
+        static void AreaOfCircle()
+        {
+            double radius = 12;
+            double area = Math.PI * Math.Pow(radius,2);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
         }
     }
 }
